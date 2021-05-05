02747d9a01b6abd3f9fe99a744bbf8ae5b43f65a
kenben77 <57472902+kenben77@users.noreply.github.com>
2021-05-05 14:30:02 -0700

    Added un-parsed population data

--- vaccination_data/retriever.py
+++ vaccination_data/retriever.py
@@ -8,6 +8,8 @@ import time
 # Web address for directory containing vaccination data.
 url_vax = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/"
 url_jhu = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/jhu/"
+url_pop =  "https://datahub.io/JohnSnowLabs/population-figures-by-country/r/"
+
 # Every 24 hours, retieve data files from the server
 while 1:
     # Retrieve each data file - vacinations, loations, manufacturers,
@@ -27,6 +29,10 @@ while 1:
     full_data = requests.get(url_jhu + "full_data.csv")
     if not full_data:
         print("error: us-state-manufacturer.csv not found")
+    pop_fig_by_country = requests.get(url_pop + "population-figures-by-country-csv.csv")
+    if not full_data:
+        print("error: us-state-manufacturer.csv not found")
+
 
     # Open, write to, and then close each data file
     vax_file = open("vaccinations.csv", "w")
@@ -34,12 +40,14 @@ while 1:
     locations_file = open("locations.csv", "w")
     us_state_vax_file = open("us-state-vaccinations.csv", "w")
     full_data_file = open("full-data.csv", "w")
+    pop_fig_by_country_file = open("population-figures-by-country-csv.csv", "w")
 
     vax_file.write(vax.text.encode("utf-8"))
     vax_by_man_file.write(vax_by_man.text.encode("utf-8"))
     locations_file.write(locations.text.encode("utf-8"))
     us_state_vax_file.write(us_state_vax.text.encode("utf-8"))
     full_data_file.write(full_data.text.encode("utf-8"))
+    pop_fig_by_country_file.write(pop_fig_by_country.text.encode("utf-8"))
 
     vax_file.close()
     vax_by_man_file.close()
