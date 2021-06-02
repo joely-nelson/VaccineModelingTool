c4819f87e40085d5f9b68ca1c376b7c9eef83378
kenben77 <57472902+kenben77@users.noreply.github.com>
2021-06-02 12:43:02 -0700

    Added print statements for diagnostics

--- vaccination_data/retriever.py
+++ vaccination_data/retriever.py
@@ -34,6 +34,7 @@ while 1:
     pop_fig_by_country = requests.get(url_pop + "population-figures-by-country-csv.csv")
     if not pop_fig_by_country:
         print("error: population-figures-by-country-csv.csv not found")
+    print("All files retrieved")
 
     # Open every csv data file, including parsed files
     vax_file = open("vaccinations.csv", "w")
@@ -54,27 +55,32 @@ while 1:
     full_data_file.write(full_data.text.encode("utf-8"))
     pop_fig_by_country_file.write(pop_fig_by_country.text.encode("utf-8"))
     latest_data_file.write(latest_data.text.encode("utf-8"))
+    print("Unparsed files written")
 
     # Parse the population file
     pop_fig_by_country_file.close()
     pop_fig_by_country_file_unparsed = open("population-figures-by-country-csv.csv", "r")
     vax_data_parser.population_parser(pop_fig_by_country_file_unparsed, 
         pop_fig_by_country_file_parsed)
+    print("Parsed population data")
 
     # Parse the vaccination file
     vax_file.close()
     vax_file_unparsed = open("vaccinations.csv", "r")
     vax_data_parser.vax_parser(vax_file_unparsed, vax_file_parsed)
+    print("Parsed vaccination data")
 
     # Parse the full_data file for cases and deaths
     full_data_file.close()
     full_data_file_unparsed = open("full-data.csv", "r")
     vax_data_parser.cases_deaths_parser(full_data_file_unparsed, death_file, case_file)
+    print("Parsed data for death and case numbers")
 
     # Parse the latest data file for r-values
     latest_data_file.close()
     latest_data_file_unparsed = open("owid-covid-latest.csv")
     vax_data_parser.r_value_parser(latest_data_file_unparsed, r_value_file)
+    print("Parsed data for reproduction values")
 
     # Convert the population file to a json
     pop_fig_by_country_file_parsed.close()
@@ -106,6 +112,7 @@ while 1:
     r_value_file = open("r-values.csv", "r")
     r_value_json = open("../json_io_files/r-values.json", "w")
     json_maker.make_json(r_value_file, r_value_json, 1, 2)
+    print("Converted all parsed data to json files")
 
     # Make a json containing a dictionary mapping country names to ISO codes
     vax_file_parsed.close()
@@ -117,6 +124,7 @@ while 1:
     iso_json = open("../json_io_files/countries-to-iso.json", "w")
     json_maker.make_iso_json(pop_fig_by_country_file_parsed, vax_file_parsed, 
         r_value_file, iso_json)
+    print("Created iso code json")
 
     # Make master json mapping iso codes to a list containing population, vaccinations,
     # deaths, and cases, in that order.
@@ -134,6 +142,7 @@ while 1:
     master_json = open("../json_io_files/master-json.json", "w")
     json_maker.make_master_json(pop_fig_by_country_json, vax_json, 
         death_json, case_json, r_value_json, iso_json, master_json)
+    print("Created master json")
 
     # Close all files that are still openvg
     vax_file_unparsed.close()
@@ -154,6 +163,8 @@ while 1:
     iso_json.close()
     master_json.close()
     print("All files closed")
+
+    print("Waiting...")
 
     # Make the loop wait 1 day (86400s) before running again
     time.sleep(86400)
